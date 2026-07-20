66fb9a411e36a6827c33a1efa333935941e3fee8
joshuamost726-dot <joshuamost726@gmail.com>
2026-07-20 10:04:18 -0700

    Update api.js

--- src/api.js
+++ src/api.js
@@ -1,1 +1,7 @@
+const BASE = process.env.REACT_APP_API_URL || "http://localhost:5000";
 
+export async function getJSON(path) {
+  const res = await fetch(`${BASE}${path}`);
+  if (!res.ok) throw new Error(`${res.status} ${res.statusText}`);
+  return res.json();
+}
